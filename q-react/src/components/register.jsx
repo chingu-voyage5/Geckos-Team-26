93ef1b4e8ad22de1536736db6e3ba7908634cc3a
hoxtygen <hoxtygen@live.com>
2018-05-29 18:08:01 +0100

    testing how things work

---
+++ q-react/src/components/register.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class register extends Component {
+	render() {
+		return (
+			<div>
+			<h3>Hello World</>
+			</div>
+		);
+	}
+}
+//Just testing things
