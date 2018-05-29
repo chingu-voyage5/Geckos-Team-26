03dd0927acb8189001b25f5f862547f5eb8399bb
Michal <majkwel@yahoo.com>
2018-05-29 13:36:45 +0200

    Updated Main Front Page Styles, Buttons. Footer

--- q-react/src/Main.js
+++ q-react/src/Main.js
@@ -24,7 +24,9 @@ const Main = () => {
       <div className='hero-left'>
         <h1>Q.<span>me</span></h1>
         <p>Welcome to the universal Quiz platform</p>
-        <a className='sign-up-btn'>Sign Up</a>
+        <Link to='/sign-up' className='sign-up-btn'>
+          <div className='sign-up-button'>Sign Up</div>
+        </Link>
       </div>
       <div className='hero-right'>
         <div className='note-image'></div>
@@ -38,11 +40,18 @@ const Main = () => {
           <p>Test your knowledge on a variety of topics by taking some of the quizes available on Q.me. Sign-up for an account to create your own quizes to share with other people.</p>
         </div>
         <div className='intro-right'>
-          <button>Create Quiz</button>
-          <button>Explore Quizes</button>
+        <Link to='/create' className='create-btn'>
+          <div className='create-button'>Create Quiz</div>
+        </Link>
+        <Link to='/quizes' className='explore-btn'>
+          <div className='explore-button'>Explore Quizes</div>
+        </Link>
         </div>
       </div>
     </section> {/***** END INTRO *****/}
+    <footer className='footer'>
+    Design & Code © 2018 by Chingu-Geckos-26   
+    </footer>
     </Fragment>
   )
 }
