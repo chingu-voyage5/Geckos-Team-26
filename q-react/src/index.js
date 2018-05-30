e7582a57294b6bd78d64d4b74deb0dad76040260
Michal <majkwel@yahoo.com>
2018-05-30 08:47:51 +0200

    Fixed CSS not rendering hero correctly in Firefox browser

--- q-react/src/index.js
+++ q-react/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import FontAwesomeIcon from 'react-fontawesome';
 import registerServiceWorker from './registerServiceWorker';
+import 'normalize.css';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
